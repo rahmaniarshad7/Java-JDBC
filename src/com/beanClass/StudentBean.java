1db5ece7c20910a4b0a2c57fa7928cf9c8d2e41f
rahmaniarshad7 <arshadrahmani7544a@gmail.com>
2024-09-02 10:20:45 +0530

    git commit -m "java commit"

---
+++ src/com/beanClass/StudentBean.java
@@ -0,0 +1,39 @@
+package com.beanClass;
+
+public class StudentBean
+{
+    private int id;
+    private int rollNo;
+    private String name;
+    private String courseName;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setCourseName(String bCa){
+        this.courseName = courseName;
+    }
+    public String getCourseName(){
+        return courseName;
+    }
+    public void setRollNo(int i){
+        this.rollNo = rollNo;
+    }
+    public int getRollNo()
+    {
+        return rollNo;
+    }
+
+}
