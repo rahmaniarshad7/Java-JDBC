13d5d9460d44fad9bf38f194d79de2c121150469
Arshad Rahmani <rahmaniarshad@outlook.com>
2026-04-12 23:58:08 +0530

    Java commit

--- untitled/src/OOPs/Name.java
+++ untitled/src/OOPs/Name.java
@@ -3,6 +3,8 @@ package OOPs;
 public class Name {
     static void main() {
             String name = "John Doe";
+            int age = 25;
             System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
     }
 }
